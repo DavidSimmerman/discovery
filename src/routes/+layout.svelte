80e3306bb85503c42dfe3449a1e38f33a35e1dba
David Simmerman <davidsimmermancs@gmail.com>
2026-05-24 14:29:01 -0400

    ui: mount PlaybackStore + MiniPlayer in root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
   import '../app.css';
+  import { page } from '$app/state';
   import favicon from '$lib/assets/favicon.svg';
+  import { createPlaybackStore, setPlaybackStore } from '$lib/playback/player.svelte';
+  import MiniPlayer from '$lib/components/MiniPlayer.svelte';
+
   let { children } = $props();
+
+  const playback = createPlaybackStore();
+  setPlaybackStore(playback);
 </script>
 
 <svelte:head>
@@ -9,3 +16,5 @@
 </svelte:head>
 
 {@render children()}
+
+<MiniPlayer store={playback} currentRoute={page.url.pathname} />
